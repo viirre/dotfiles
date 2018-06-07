2859317042de58e0bfedfb19b818100194107f74
Victor Eliasson <victor@adaptivemedia.se>
2018-06-07 23:30:04 +0200

    updated

--- install.sh
+++ install.sh
@@ -17,18 +17,34 @@ brew bundle
 # Make ZSH the default shell environment
 chsh -s $(which zsh)
 
+# Install PHP extensions with PECL
+pecl install xdebug
+pecl install redis
+pecl install imagick
+
 # Install Composer
 curl -sS https://getcomposer.org/installer | php
 mv composer.phar /usr/local/bin/composer
 
 # Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/lumen-installer laravel/valet tightenco/jigsaw
+/usr/local/bin/composer global require laravel/installer laravel/valet
 
 # Install Laravel Valet
 $HOME/.composer/vendor/bin/valet install
 
-# Create a Code directory
-mkdir $HOME/Code
+# Install global NPM packages
+#npm install --global yarn
+
+# Create a Sites directory
+# This is a default directory for macOS user accounts but doesn't comes pre-installed
+mkdir $HOME/Sites
+
+# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
+rm -rf $HOME/.zshrc
+ln -s $HOME/.dotfiles/.zshrc $HOME/.zshrc
+
+# Symlink the Mackup config file to the home directory
+ln -s $HOME/.dotfiles/.mackup.cfg $HOME/.mackup.cfg
 
 # Set macOS preferences
 # We will run this last because this will reload the shell
